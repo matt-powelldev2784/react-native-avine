11276d3aa270c3850789df4aa02e29ee69ca7738
Matthew Powell <matt.powell2784@gmail.com>
2023-10-02 20:36:24 +0100

    build: initial commit react native and tailwind installed

--- App.tsx
+++ App.tsx
@@ -4,10 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className="bg-red-500">
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
