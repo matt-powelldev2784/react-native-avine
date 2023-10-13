8ffb8df4a9bf9981e735c4bd0f34efab859cc8a2
Matthew Powell <matt.powell2784@gmail.com>
2023-10-13 18:13:16 +0100

    feat: display map in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,143 +1,42 @@
-import { useCallback, useMemo, useRef, useState } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
-import MapView, { Marker } from 'react-native-maps'
-import { ClusterProps, MarkerClusterer } from '@teovilla/react-native-web-maps'
-import type { Region } from 'react-native-maps'
+import React from 'react'
+import MapView, { Polyline } from 'react-native-maps'
 
-function MyClusterComponent(props: ClusterProps<{ onPress(): void }>) {
-  console.log(
-    'process.env.GOOGLE_MAPS_API_KEY',
-    process.env.GOOGLE_MAPS_API_KEY
-  )
+const MyMapComponent = () => {
+  const coordinates = [
+    { latitude: 37.8025259, longitude: -122.4351431 },
+    { latitude: 37.7896386, longitude: -122.421646 },
+    { latitude: 37.7665248, longitude: -122.4161628 },
+    // Add more coordinates as needed
+  ]
 
   return (
-    <Marker
-      onPress={props.onPress}
-      coordinate={props.coordinate}
-      anchor={{ x: 0.5, y: 0.5 }}
+    <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      provider="google"
+      googleMapsApiKey=""
     >
-      <View style={styles.cluster}>
-        <Text style={styles.clusterText}>{props.pointCountAbbreviated}</Text>
-      </View>
-    </Marker>
+      <Polyline
+        coordinates={coordinates}
+        strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
+        strokeColors={[
+          '#7F0000',
+          '#00000000', // no color, creates a "long" gradient between the previous and next coordinate
+          '#B24112',
+          '#E5845C',
+          '#238C23',
+          '#7F0000',
+          // add more colors for your polylines as needed
+        ]}
+        strokeWidth={6}
+      />
+    </MapView>
   )
 }
 
-export default function App() {
-  const [region, setRegion] = useState<Region | null>(null)
-
-  const mapRef = useRef<MapView>(null)
-
-  const loadingFallback = useMemo(() => {
-    return (
-      <View>
-        <Text>Loading</Text>
-      </View>
-    )
-  }, [])
-
-  return (
-    <View style={styles.container}>
-      <MapView
-        ref={mapRef}
-        provider="google"
-        style={{ flex: 1 }}
-        onRegionChange={setRegion}
-        loadingFallback={loadingFallback}
-        googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY}
-      >
-        <MarkerClusterer
-          region={region}
-          renderCluster={(cluster) => (
-            <MyClusterComponent
-              {...cluster}
-              onPress={() =>
-                mapRef.current?.animateCamera({
-                  center: cluster.coordinate,
-                  zoom: cluster.expansionZoom + 3,
-                })
-              }
-            />
-          )}
-        >
-          <Marker
-            coordinate={{
-              latitude: 59.33956246905637,
-              longitude: 18.050015441134114,
-            }}
-          />
-          <Marker
-            coordinate={{
-              latitude: 59.3442016958775,
-              longitude: 18.038256636812825,
-            }}
-          />
-        </MarkerClusterer>
-        {/* {SHOW_LOCATION_BUBBLE && position && (
-            <>
-              <Marker
-                anchor={{ x: 0.5, y: 1 }}
-                coordinate={{
-                  latitude: position.coords.latitude,
-                  longitude: position.coords.longitude,
-                }}
-              >
-                <View
-                  style={{
-                    paddingHorizontal: 10,
-                    paddingVertical: 5,
-                    borderRadius: 999,
-                    backgroundColor: 'salmon',
-                    position: 'relative',
-                    zIndex: 19,
-                  }}
-                >
-                  <View
-                    style={{
-                      position: 'absolute',
-                      width: 10,
-                      height: 10,
-                      backgroundColor: 'salmon',
-                      zIndex: 20,
-                      bottom: -5,
-                      alignSelf: 'center',
-                      transform: [{ rotate: '45deg' }],
-                    }}
-                  ></View>
-                  <Text>You are here</Text>
-                </View>
-                {Platform.select({
-                  native: <View style={{ height: 7 }}></View>,
-                })}
-              </Marker>
-              <Circle
-                center={{
-                  latitude: position.coords.latitude,
-                  longitude: position.coords.longitude,
-                }}
-                fillColor="rgba(0,0,0,0.5)"
-                radius={100}
-              />
-            </>
-          )} */}
-      </MapView>
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  cluster: {
-    backgroundColor: 'salmon',
-    width: 20,
-    height: 20,
-    borderRadius: 999,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  clusterText: {
-    fontWeight: '700',
-  },
-})
+export default MyMapComponent
