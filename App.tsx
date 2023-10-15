fe9f29a9a9195c3dc034e4359b58153406112456
Matthew Powell <matt.powell2784@gmail.com>
2023-10-15 19:22:09 +0100

    feat: sucessfully render google maps web view

--- App.tsx
+++ App.tsx
@@ -1,41 +1,60 @@
 import React from 'react'
-import MapView, { Polyline, Marker } from 'react-native-maps'
+import { View } from 'react-native'
+import {
+  GoogleMap,
+  Polyline,
+  Marker,
+  useLoadScript,
+} from '@react-google-maps/api'
+import { Platform, StyleSheet } from 'react-native'
+
+const center = {
+  lat: 37.78825,
+  lng: -122.4324,
+}
+
+const pathCoordinates = [
+  { lat: 37.8025259, lng: -122.4351431 },
+  { lat: 37.7896386, lng: -122.421646 },
+  { lat: 37.7665248, lng: -122.4161628 },
+  // Add more coordinates as needed
+]
+
+const pathOptions = {
+  strokeColor: '#000',
+  strokeOpacity: 1,
+  strokeWeight: 6,
+}
 
 const MyMapComponent = () => {
-  const coordinates = [
-    { latitude: 37.8025259, longitude: -122.4351431 },
-    { latitude: 37.7896386, longitude: -122.421646 },
-    { latitude: 37.7665248, longitude: -122.4161628 },
-    // Add more coordinates as needed
-  ]
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY || '',
+  })
+
+  if (!isLoaded) return <div>Loading...</div>
 
   return (
-    <MapView
-      style={{ flex: 1 }}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-      provider="google"
-      googleMapsApiKey={process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY}
-    >
-      <Polyline
-        coordinates={coordinates}
-        strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
-        strokeColors={
-          [
-            // add more colors for your polylines as needed
-          ]
-        }
-        strokeWidth={6}
-      />
-      <Marker coordinate={coordinates[0]} />
-      <Marker coordinate={coordinates[1]} />
-      <Marker coordinate={coordinates[2]} />
-    </MapView>
+    <View style={styles.mapContainer}>
+      map
+      <GoogleMap
+        zoom={10}
+        center={center}
+        mapContainerStyle={{ width: '100%', height: '100%' }}
+      >
+        {/* <Polyline path={pathCoordinates} options={pathOptions} />
+      {pathCoordinates.map((coordinate, i) => (
+        <Marker key={i} position={coordinate} />
+      ))} */}
+      </GoogleMap>
+    </View>
   )
 }
 
 export default MyMapComponent
+
+const styles = StyleSheet.create({
+  mapContainer: {
+    width: '100%',
+    height: '100%',
+  },
+})
