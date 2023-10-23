383a43e0ab2705a22bdabe116369664edd6a4745
Matthew Powell <matt.powell2784@gmail.com>
2023-10-23 21:05:59 +0100

    feat: add map screen

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,17 +1,9 @@
 import { Platform, View } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
-import { StackNavigationProp } from '@react-navigation/stack'
-import { RootStackParamList } from '../StackNavigator'
 import React from 'react'
 import WebMap from '../components/WebMap'
 import NativeMap from '../components/NativeMap'
-import { useLayoutEffect } from 'react'
-
-type NavigationProp = StackNavigationProp<RootStackParamList, 'Map'>
 
 const MapScreen = () => {
-  const navigation = useNavigation<NavigationProp>()
-
   const Map = Platform.OS === 'web' ? <WebMap /> : <NativeMap />
 
   return Map
