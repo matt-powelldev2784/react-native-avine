9b46bc4507b81f738cbb632386c92629c891cb0c
Matthew Powell <matt.powell2784@gmail.com>
2023-11-19 16:03:36 +0000

    refactor: split dashboard code to native and web components

--- app/components/dashboard/Dashboard.tsx
+++ app/components/dashboard/Dashboard.tsx
@@ -2,14 +2,13 @@ import {
   View,
   StyleSheet,
   Platform,
-  Text,
   TouchableOpacity,
+  Image,
 } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import React, { ReactNode } from 'react'
 import PlanMeLogo from '../PlanMeLogo/PlanMeLogo'
 import NavBar from '../navBar/NavBar'
-import { useAuth } from '../auth/AuthProvider'
 import { useNavigation } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
 import { RootStackParamList } from '../../screens/stackNavigator/StackNavigator'
@@ -21,12 +20,7 @@ interface DashboardProps {
 
 const Dashboard = ({ children }: DashboardProps) => {
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>()
-  const { isLargeWeb, isSmallWeb, isNative } = useDeviceType()
-  const { userInfo } = useAuth()
-  const userInitials = userInfo?.displayName
-    .split(' ')
-    .map((word: string) => word.charAt(0))
-    .join('')
+  const { isLargeWeb, isSmallWeb } = useDeviceType()
 
   return (
     <SafeAreaView style={styles.screen}>
@@ -40,9 +34,10 @@ const Dashboard = ({ children }: DashboardProps) => {
               style={styles.button}
               onPress={() => navigation.navigate('SignOut')}
             >
-              <View style={styles.circle}>
-                <Text style={styles.account}>{userInitials || null}</Text>
-              </View>
+              <Image
+                source={require('../../../assets/settings.png')}
+                style={{ width: 25, height: 25 }}
+              />
             </TouchableOpacity>
           </View>
         </View>
@@ -52,43 +47,20 @@ const Dashboard = ({ children }: DashboardProps) => {
       {isLargeWeb ? (
         <View style={styles.headerLargeScreen}>
           <View style={styles.logoContainerLargeScreen}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={() => navigation.navigate('SignOut')}
-            >
-              <View style={styles.circle}>
-                <Text style={styles.account}>{userInitials || null}</Text>
-              </View>
+            <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
+              <PlanMeLogo width={200} height={50} />
             </TouchableOpacity>
-            <PlanMeLogo width={200} height={50} />
           </View>
 
           <NavBar />
         </View>
       ) : null}
 
-      {/* -------------------------- Native App View --------------------------  */}
-      {isNative ? (
-        <View style={styles.headerNative}>
-          <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
-            <PlanMeLogo width={150} height={40} />
-          </TouchableOpacity>
-          <View style={styles.circle}>
-            <Text
-              style={styles.account}
-              onPress={() => navigation.navigate('SignOut')}
-            >
-              {userInitials || null}
-            </Text>
-          </View>
-        </View>
-      ) : null}
-
       {/* -------------------------- Children for Main View --------------------------  */}
       <View style={styles.page}>{children}</View>
 
-      {/* -------------------------- Navbar For Native App --------------------------  */}
-      {isNative || isSmallWeb ? <NavBar /> : null}
+      {/* -------------------------- Navbar For Smnall Devices --------------------------  */}
+      {isSmallWeb ? <NavBar /> : null}
     </SafeAreaView>
   )
 }
@@ -112,15 +84,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: Platform.OS === 'web' ? 24 : 0,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%',
-  },
-  headerNative: {
-    backgroundColor: '#337bae',
-    paddingHorizontal: Platform.OS === 'web' ? 24 : 16,
-    paddingVertical: Platform.OS === 'web' ? 0 : 6,
-    flexDirection: 'row',
-    justifyContent: Platform.OS === 'web' ? 'center' : 'space-between',
     alignItems: 'center',
     width: '100%',
   },
@@ -148,7 +111,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     padding: 8,
   },
-  circle: {
+  circleSmallScreen: {
     width: 35,
     height: 35,
     borderRadius: 50,
@@ -156,7 +119,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  account: { color: '#337bae', fontWeight: 'bold' },
+  circleLargeScreen: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    backgroundColor: 'skyblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  account: {
+    color: '#337bae',
+    fontWeight: 'bold',
+  },
+  accountLarge: {
+    color: '#337bae',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
   button: {
     alignItems: 'center',
     justifyContent: 'center',
