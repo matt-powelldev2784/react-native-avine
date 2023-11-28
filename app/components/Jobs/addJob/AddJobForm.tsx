77baaab6988a8f17ee24e768c9c6979731aa9f1f
Matthew Powell <matt.powell2784@gmail.com>
2023-11-28 20:08:03 +0000

    feat: add state to form for three part process

--- app/components/Jobs/addJob/AddJobForm.tsx
+++ app/components/Jobs/addJob/AddJobForm.tsx
@@ -1,107 +1,144 @@
-import React from 'react'
-import { TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native'
+import React, { useState } from 'react'
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native'
 import useFormikProps from './hooks/useFormikProps'
 import InputField from './components/InputField'
 import Dropdown from './components/DropDown'
 
 const AddJobForm = () => {
-  const formik = useFormikProps()
+  const [activeStep, setActiveStep] = useState(0)
+  const formik = useFormikProps(activeStep)
+  const moveToNextStepIfFormIsValid = () => {
+    if (formik.isValid) {
+      setActiveStep((prev) => {
+        return prev + 1
+      })
+    }
+  }
 
   return (
     <section style={styles.wrapper}>
       <div style={styles.formContainer}>
         <View style={styles.titleContainer}>
-          <Text style={styles.titleActive}>Add Place Details</Text>
+          <Text style={styles.titleActive}>Add Location Details</Text>
           <View style={styles.line} />
           <Text style={styles.title}>Add Contact Details</Text>
           <View style={styles.line} />
           <Text style={styles.title}>Add Job Details</Text>
         </View>
 
-        <InputField
-          formik={formik}
-          name="jobName"
-          placeholder="Job Name"
-          title="Job Name"
-          imageName={'wiper'}
-        />
-        <InputField
-          formik={formik}
-          name="address"
-          placeholder="Address"
-          title="Job Address"
-          imageName={'location'}
-        />
-        <InputField
-          formik={formik}
-          name="postcode"
-          placeholder="Post Code"
-          title="Post Code"
-          imageName={'locationCircle'}
-        />
-        {/* <InputField
-          formik={formik}
-          name="contactName"
-          placeholder="Contact Name"
-          title="Contact Name"
-          imageName={'person'}
-        />
-        <InputField
-          formik={formik}
-          name="contactTel"
-          placeholder="Contact Telephone Number"
-          title="Contact Telephone Number"
-          numericInput={true}
-          imageName={'tel'}
-        />
+        {/*********************  Step 1 ***************************/}
+        {activeStep === 0 ? (
+          <>
+            <InputField
+              formik={formik}
+              name="jobName"
+              placeholder="Job Name"
+              title="Job Name"
+              imageName={'wiper'}
+            />
+            <InputField
+              formik={formik}
+              name="address"
+              placeholder="Address"
+              title="Job Address"
+              imageName={'location'}
+            />
+            <InputField
+              formik={formik}
+              name="postcode"
+              placeholder="Post Code"
+              title="Post Code"
+              imageName={'locationCircle'}
+            />
+          </>
+        ) : null}
 
-        <InputField
-          formik={formik}
-          name="jobType"
-          placeholder="Job Type e.g Front Only"
-          title="Job Type"
-          imageName={'diamond'}
-        />
-        <InputField
-          formik={formik}
-          name="time"
-          placeholder="Estimated Job Time In Hours e.g 1.5"
-          title="Estimated Job Time"
-          numericInput={true}
-          imageName={'clock'}
-        />
-        <InputField
-          formik={formik}
-          name="price"
-          placeholder="Price"
-          title="Price"
-          numericInput={true}
-          imageName={'poundSign'}
-        />
-        <Dropdown
-          formik={formik}
-          name="frequency"
-          placeholder="Cleaning Frequency"
-          title="Cleaning Frequency"
-          options={['Daily', 'Weekly', 'Monthly', '2 Monthly', '3 Monthly']}
-          imageName={'calender'}
-        />
-        <InputField
-          formik={formik}
-          name="notes"
-          placeholder="Notes"
-          title="Notes"
-          numericInput={true}
-          imageName={'notes'}
-        /> */}
+        {/*********************  Step 2 ***************************/}
+        {activeStep === 1 ? (
+          <>
+            <InputField
+              formik={formik}
+              name="contactName"
+              placeholder="Contact Name"
+              title="Contact Name"
+              imageName={'person'}
+            />
+            <InputField
+              formik={formik}
+              name="contactTel"
+              placeholder="Contact Telephone Number"
+              title="Contact Telephone Number"
+              numericInput={true}
+              imageName={'tel'}
+            />
+          </>
+        ) : null}
+
+        {/*********************  Step 3 ***************************/}
+        {activeStep === 2 ? (
+          <>
+            <InputField
+              formik={formik}
+              name="jobType"
+              placeholder="Job Type e.g Front Only"
+              title="Job Type"
+              imageName={'diamond'}
+            />
+            <InputField
+              formik={formik}
+              name="time"
+              placeholder="Estimated Job Time In Hours e.g 1.5"
+              title="Estimated Job Time"
+              numericInput={true}
+              imageName={'clock'}
+            />
+            <InputField
+              formik={formik}
+              name="price"
+              placeholder="Price"
+              title="Price"
+              numericInput={true}
+              imageName={'poundSign'}
+            />
+            <Dropdown
+              formik={formik}
+              name="frequency"
+              placeholder="Cleaning Frequency"
+              title="Cleaning Frequency"
+              options={['Daily', 'Weekly', 'Monthly', '2 Monthly', '3 Monthly']}
+              imageName={'calender'}
+            />
+            <InputField
+              formik={formik}
+              name="notes"
+              placeholder="Notes"
+              title="Notes"
+              numericInput={true}
+              imageName={'notes'}
+            />
+          </>
+        ) : null}
 
         <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            onPress={() => formik.handleSubmit()}
-            style={styles.buttonSubmit}
-          >
-            <Text style={styles.buttonText}>Next</Text>
-          </TouchableOpacity>
+          {activeStep < 2 ? (
+            <>
+              <TouchableOpacity
+                onPress={moveToNextStepIfFormIsValid}
+                style={styles.button}
+              >
+                <Text style={styles.buttonText}>Next</Text>
+              </TouchableOpacity>
+            </>
+          ) : null}
+
+          {activeStep === 2 ? (
+            <TouchableOpacity
+              onPress={() => formik.handleSubmit()}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>Add Job</Text>
+            </TouchableOpacity>
+          ) : null}
         </View>
       </div>
     </section>
@@ -169,7 +206,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     maxWidth: 600,
   },
-  buttonSubmit: {
+  button: {
     alignItems: 'center',
     backgroundColor: '#337bae',
     padding: 10,
