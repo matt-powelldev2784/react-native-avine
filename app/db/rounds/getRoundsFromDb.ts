65341d763773d995b552380519c5367b5f50a417
Matthew Powell <matt.powell2784@gmail.com>
2023-12-08 20:53:51 +0000

    feat: add get rounds from db function

--- app/db/rounds/getRoundsFromDb.ts
+++ app/db/rounds/getRoundsFromDb.ts
@@ -1,54 +1,46 @@
 import { doc, getDoc, collection, getDocs } from 'firebase/firestore'
 import { db, auth } from '../../../firebaseConfig'
+import { DocumentData } from '@google-cloud/firestore'
 
-export const getRoundsFromDb = async () => {
-  if (auth.currentUser === null) {
-    return
+type Round = {
+  roundData: DocumentData
+  jobs: DocumentData[]
+}
+
+const processJobsData = async (userDoc: any, roundData: any) => {
+  const jobs = []
+
+  for (const job of roundData.jobs) {
+    const jobDocRef = doc(userDoc, 'jobs', job.id)
+    const jobDoc = await getDoc(jobDocRef)
+    const jobData = jobDoc.data()
+    if (jobData) {
+      jobs.push(jobData)
+    }
   }
+  return jobs
+}
+
+export const getRoundsFromDb = async (): Promise<Round[]> => {
+  if (auth.currentUser === null) {
+    return []
+  }
+  const collatedRoundData: Round[] = []
 
   const userDoc = doc(db, 'users', auth.currentUser.uid)
   const roundsCollection = collection(userDoc, 'rounds')
   const roundDocsSnapshot = await getDocs(roundsCollection)
 
-  // const rounds = []
-  // for (const roundDoc of roundDocsSnapshot.docs) {
-  //   const roundData = roundDoc.data()
-
-  //   // Convert job references to job data
-  //   const jobs = []
-  //   for (const jobRef of roundData.jobs) {
-  //     const jobSnapshot = await getDoc(jobRef)
-  //     jobs.push(jobSnapshot.data())
-  //   }
-
-  //   rounds.push({
-  //     ...roundData,
-  //     jobs,
-  //   })
-  // }
-
-  const rounds = []
   for (const roundDoc of roundDocsSnapshot.docs) {
     const roundData = roundDoc.data()
-
-    // Convert job references to job data
-    const jobs = []
-    for (const jobRef of roundData.jobs) {
-      try {
-        const jobSnapshot = await getDoc(jobRef)
-        jobs.push(jobSnapshot.data())
-      } catch (error) {
-        console.error('Error fetching job data:', error)
-        console.log('Job reference:', jobRef)
-      }
+    const jobs = await processJobsData(userDoc, roundData)
+    const round: Round = {
+      roundData: roundData,
+      jobs: jobs,
     }
-
-    rounds.push({
-      ...roundData,
-      jobs,
-    })
+    collatedRoundData.push(round)
   }
 
-  console.log('Rounds retrieved:', roundDocsSnapshot)
-  return roundDocsSnapshot
+  console.log('Rounds retrieved:', collatedRoundData)
+  return collatedRoundData
 }
