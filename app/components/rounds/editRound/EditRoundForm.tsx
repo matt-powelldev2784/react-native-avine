5d4e15a2ac04b479780677f22a6cbe77b002b202
Matthew Powell <matt.powell2784@gmail.com>
2023-12-10 20:50:10 +0000

    feat: change frequency options

--- app/components/rounds/editRound/EditRoundForm.tsx
+++ app/components/rounds/editRound/EditRoundForm.tsx
@@ -79,9 +79,12 @@ const EditRoundForm = () => {
               options={[
                 { label: 'Daily', value: 'Daily' },
                 { label: 'Weekly', value: 'Weekly' },
+                { label: 'Bi-Weekly', value: 'Bi-Weekly' },
                 { label: 'Monthly', value: 'Monthly' },
                 { label: '2 Monthly', value: '2 Monthly' },
                 { label: '3 Monthly', value: '3 Monthly' },
+                { label: '6 Monthly', value: '6 Monthly' },
+                { label: 'Yearly', value: 'Yearly' },
               ]}
               imageName={'calender'}
             />
