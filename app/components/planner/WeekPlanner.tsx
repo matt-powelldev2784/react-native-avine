02543a60724deac21d92f6d5fea5783cc46fe931
Matthew Powell <matt.powell2784@gmail.com>
2023-12-11 21:24:44 +0000

    feat: add initial date picker styles

--- app/components/planner/WeekPlanner.tsx
+++ app/components/planner/WeekPlanner.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import {
   addDays,
@@ -7,10 +7,11 @@ import {
   format,
   subDays,
 } from 'date-fns'
+import theme from '../../utils/theme/theme'
 
 const weeksToDisplay = eachWeekOfInterval(
   {
-    start: subDays(new Date(), 28),
+    start: subDays(new Date(), 64),
     end: addDays(new Date(), 364),
   },
   { weekStartsOn: 1 },
@@ -23,43 +24,94 @@ const weeksToDisplay = eachWeekOfInterval(
 
 const WeekPlanner = () => {
   const weekCalander = weeksToDisplay.map((week) => {
-    return (
-      <View style={styles.weekContainer} key={week[0].toString()}>
-        {week.map((day) => {
-          const weekDay = format(day, 'EEEEEE')
+    const weekStartMonth = format(week[0], 'MMMM')
+    const weekEndMonth = format(week[week.length - 1], 'MMMM')
+    const monthString =
+      weekStartMonth === weekEndMonth
+        ? weekStartMonth
+        : `${weekStartMonth} / ${weekEndMonth}`
 
-          return (
-            <View style={styles.dayContainer} key={day.toString()}>
-              <Text>{weekDay}</Text>
-              <Text>{day.getDate()}</Text>
-            </View>
-          )
-        })}
+    return (
+      <View style={styles.datePickerWrapper} key={week[0].toString()}>
+        <View style={styles.monthAndWeekContainer}>
+          <Text style={styles.monthName}>{monthString}</Text>
+
+          <View style={styles.dayWrapper}>
+            <Image
+              source={require('../../../assets/left_arrow.png')}
+              style={{ width: 25, height: 25 }}
+            />
+
+            {week.map((day) => {
+              const weekDay = format(day, 'EEEEEE')
+
+              return (
+                <View style={styles.dayContainer} key={day.toString()}>
+                  <Text>{weekDay}</Text>
+                  <Text>{day.getDate()}</Text>
+                  <Image
+                    source={require('../../../assets/dot.png')}
+                    style={{ width: 5, height: 5, marginVertical: 2 }}
+                  />
+                </View>
+              )
+            })}
+
+            <Image
+              source={require('../../../assets/right_arrow.png')}
+              style={{ width: 25, height: 25 }}
+            />
+          </View>
+        </View>
       </View>
     )
   })
 
-  return (
-    <View style={styles.conatiner}>
-      <Text>Planner</Text>
-      {weekCalander}
-    </View>
-  )
+  return <View style={styles.conatiner}>{weekCalander}</View>
 }
 
 const styles = StyleSheet.create({
   conatiner: {
     width: '100%',
   },
-  weekContainer: {
+  datePickerWrapper: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: 'red',
+    backgroundColor: theme.colors.formFlowSecondary,
+    paddingBottom: 8,
+    marginBottom: 100,
+    paddingHorizontal: 8,
+  },
+  monthAndWeekContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+  monthName: {
+    color: theme.colors.primary,
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  dayWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
   },
   dayContainer: {
     alignItems: 'center',
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    borderRadius: 8,
+    width: 40,
+  },
+  dayContainerSelected: {
+    alignItems: 'center',
+    backgroundColor: theme.colors.plannerPrimary,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    borderRadius: 8,
+    width: 40,
   },
 })
 
