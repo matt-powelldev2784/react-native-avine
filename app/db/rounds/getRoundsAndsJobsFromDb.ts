57c1fe0582425849cfd72c652e6ad57fdf649e01
Matthew Powell <matt.powell2784@gmail.com>
2023-12-21 17:42:19 +0000

    feat: create seperate routes from rounds and rounds with jobs

--- app/db/rounds/getRoundsAndsJobsFromDb.ts
+++ app/db/rounds/getRoundsAndsJobsFromDb.ts
@@ -1,23 +1,27 @@
 import { doc, collection, getDocs, query } from 'firebase/firestore'
 import { db, auth } from '../../../firebaseConfig'
-import { RoundNoJobsT } from '../../types/RoundT'
+import { getUserJobsFromDb } from '../jobs/getUserJobsFromDb'
+import { RoundWithJobT } from '../../types/RoundT'
 
-export const getRoundsFromDb = async (): Promise<RoundNoJobsT[]> => {
+export const getRoundsAndJobsFromDb = async (): Promise<RoundWithJobT[]> => {
   if (auth.currentUser === null) {
     return []
   }
 
   try {
+    const jobData = await getUserJobsFromDb()
+
     const userDoc = doc(db, 'users', auth.currentUser.uid)
     const roundsCollection = collection(userDoc, 'rounds')
     const roundsQuery = query(roundsCollection)
     const roundsSnapshot = await getDocs(roundsQuery)
 
-    const roundsData: RoundNoJobsT[] = roundsSnapshot.docs.map((round) => ({
+    const roundsData: RoundWithJobT[] = roundsSnapshot.docs.map((round) => ({
       id: round.id,
       roundName: round.data().roundName,
       location: round.data().location,
       frequency: round.data().frequency,
+      jobs: jobData?.filter((job) => job.linkedRounds.includes(round.id)),
     }))
 
     roundsData.sort((a, b) => a.roundName.localeCompare(b.roundName))
