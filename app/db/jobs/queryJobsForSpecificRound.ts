6971f4a0fbe399bd3b7b412c1595c31a7a6fb0f4
Matthew Powell <matt.powell2784@gmail.com>
2023-12-30 13:25:36 +0000

    feat: add query for jobs related to specific round

---
+++ app/db/jobs/queryJobsForSpecificRound.ts
@@ -0,0 +1,26 @@
+import { collection, query, where, getDocs } from 'firebase/firestore'
+import { db, auth } from '../../../firebaseConfig'
+import { JobWithIdT } from '../../types/JobT'
+
+export const queryJobsForSpecificRound = async (roundId: string) => {
+  if (auth.currentUser === null) {
+    return
+  }
+
+  try {
+    const jobsRef = collection(db, 'users', auth.currentUser.uid, 'jobs')
+    const q = query(jobsRef, where('linkedRounds', 'array-contains', roundId))
+
+    const querySnapshot = await getDocs(q)
+
+    const jobs: JobWithIdT[] = []
+    querySnapshot.forEach((job) => {
+      jobs.push(job.data() as JobWithIdT)
+    })
+
+    console.log('Rounds related to specific round:', jobs)
+    return jobs
+  } catch (error) {
+    console.error('Error querying rounds:', error)
+  }
+}
