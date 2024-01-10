d944c0163cb1031068e4188301988c8c5efbbb57
Matthew Powell <matt.powell2784@gmail.com>
2024-01-10 17:20:39 +0000

    feat: amend db date convertor utils to return dates with spaces

--- app/utils/convertDbDateToPlannerDate.ts
+++ app/utils/convertDbDateToPlannerDate.ts
@@ -1,7 +1,7 @@
 export const convertDbDateToPlannerDate = (dbDate: string): Date => {
-  const dayOfMonth = Number(dbDate.split('/')[0])
-  const month = Number(dbDate.split('/')[1]) - 1
-  const year = Number(dbDate.split('/')[2])
+  const dayOfMonth = Number(dbDate.split(' ')[0])
+  const month = Number(dbDate.split(' ')[1]) - 1
+  const year = Number(dbDate.split(' ')[2])
   const parsedDate = new Date(year, month, dayOfMonth)
 
   return parsedDate
