ba6690eb990687ff5cdb9f6df2d8d8088c306166
Matthew Powell <matt.powell2784@gmail.com>
2024-03-03 19:20:38 +0000

    fix: fix no scheduled round components css to make view work correctly

--- app/components/planner/weekPlanner/components/scheduledRounds/components/NoScheduledRounds.tsx
+++ app/components/planner/weekPlanner/components/scheduledRounds/components/NoScheduledRounds.tsx
@@ -21,8 +21,6 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     width: '100%',
-    height: '100%',
-    overflow: 'hidden',
   },
   flexContainer: {
     alignItems: 'center',
