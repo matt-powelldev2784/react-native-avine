e5141a9cdf955cbf1c5e8eb264226a31139b8669
Matthew Powell <matt.powell2784@gmail.com>
2024-03-10 20:44:54 +0000

    fix: get client return now fixed

--- app/db/clients/getClient.ts
+++ app/db/clients/getClient.ts
@@ -8,15 +8,15 @@ export const getClient = async (clientId: string): Promise<ClientWithIdT> => {
     return authError({ filename: 'getClient' })
   }
 
-  try {
-    const userDoc = doc(db, 'users', auth.currentUser.uid)
-    const clientDocRef = doc(userDoc, 'clients', clientId)
+try {
+  const userDoc = doc(db, 'users', auth.currentUser.uid)
+  const clientDocRef = doc(userDoc, 'clients', clientId)
 
-    const clientDoc = await getDoc(clientDocRef)
-    const clientData = clientDoc.data() as ClientWithIdT
+  const clientDoc = await getDoc(clientDocRef)
+  const clientData = clientDoc.data() as ClientWithIdT
 
-    return { ...clientData, id: clientData.id }
-  } catch (error) {
-    throw new Error(`Error getting job at getClient route: ${error}`)
-  }
+  return { ...clientData, id: clientData.id }
+} catch (error) {
+  throw new Error(`Error getting client at getClient route: ${error}`)
+}
 }
