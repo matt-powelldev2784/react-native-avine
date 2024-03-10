076f2eee28852bc87ee8a9cfdcd1aadc44be0deb
Matthew Powell <matt.powell2784@gmail.com>
2024-03-10 20:11:46 +0000

    feat: add update client db function

---
+++ app/db/clients/updateClient.ts
@@ -0,0 +1,39 @@
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
+import { db, auth } from '../../../firebaseConfig'
+import { authError } from '../authError'
+import { ClientWithIdT } from '../../types/ClientT'
+
+export const updateClient = async (clientData: ClientWithIdT) => {
+  if (auth.currentUser === null) {
+    return authError({ filename: 'updateClient' })
+  }
+
+  try {
+    const clientDocRef = doc(
+      db,
+      'users',
+      auth.currentUser.uid,
+      'clients',
+      clientData.id,
+    )
+
+    await updateDoc(clientDocRef, {
+      name: clientData.name,
+      address: clientData.address,
+      town: clientData.town,
+      postcode: clientData.postcode,
+      contactTel: clientData.contactTel,
+      notes: clientData.notes,
+      isDeleted: clientData.isDeleted,
+    })
+
+    const updatedClient = await getDoc(clientDocRef)
+    const updatedClientData = { id: updatedClient.id, ...updatedClient.data() }
+
+    return updatedClientData
+  } catch (error) {
+    throw new Error(
+      `Error updating client detail at updateClient route: ${error}`,
+    )
+  }
+}
