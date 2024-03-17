8c01d20c04204bacb1a93db8b2c05ab8ca4305dd
Matthew Powell <matt.powell2784@gmail.com>
2024-03-17 14:52:58 +0000

    feat: add error handling for toggle job is paid and job is complete

--- app/db/jobs/toggleJobIsComplete.ts
+++ app/db/jobs/toggleJobIsComplete.ts
@@ -1,4 +1,10 @@
-import { doc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore'
+import {
+  doc,
+  updateDoc,
+  arrayUnion,
+  arrayRemove,
+  getDoc,
+} from 'firebase/firestore'
 import { db, auth } from '../../../firebaseConfig'
 import { authError } from '../authError'
 import { addInvoice } from '../invoice/addInvoice'
@@ -27,6 +33,21 @@ export const toggleJobIsComplete = async ({
       'planner',
       plannerDate,
     )
+    // only allow job is complete toggle if invoice is not paid
+    const plannerDoc = await getDoc(plannerDateDocRef)
+    if (!plannerDoc.exists()) {
+      throw new Error(
+        `Error updating job is complete status at toggleInvoiceIsPaid route: planner date doc does not exist`,
+      )
+    }
+
+    const jobIsInvoiced = plannerDoc.data().invoicedJobs.includes(jobId)
+
+    if (jobIsInvoiced === true) {
+      throw new Error(
+        'You cannnot toggle a job as incomplete if the invoice has been paid.',
+      )
+    }
 
     if (isComplete === true) {
       await updateDoc(plannerDateDocRef, {
