2dbf12b91affab27da563a63b60a04f6066e5251
Matthew Powell <matt.powell2784@gmail.com>
2024-03-17 19:54:26 +0000

    feat: add invoice with id type

---
+++ app/types/InvoiceT.ts
@@ -0,0 +1,11 @@
+import { JobWithIdT } from './JobT'
+
+export interface InvoiceWithIdT {
+  id: string
+  relatedRound: string
+  relatedJob: string
+  roundType: string
+  completedDate: string
+  job: JobWithIdT
+  isPaid: boolean
+}
