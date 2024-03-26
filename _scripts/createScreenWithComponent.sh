15248e9accd7847a2eb66babcebd911a77dca32f
Matthew Powell <matt.powell2784@gmail.com>
2024-03-26 20:05:56 +0000

    feat: add script to create srceen and empty component

---
+++ _scripts/createScreenWithComponent.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# Input parameters
+folderName="invoices"
+ScreenName="AddCompanyInfo"
+ComponentName="AddCompanyInfoForm"
+componentFolderName="addCompanyInfoForm"
+navigateTo="navigateTo"
+buttonText="buttonText"
+screenTitle="Add Company Information"
+importComment="invoices"
+
+# Create the directory if it doesn't exist
+mkdir -p "./app/screens/$folderName"
+mkdir -p "./app/components/$folderName/$componentFolderName"
+
+# Create the .tsx file with the specified content
+cat << EOF > "./app/screens/$folderName/$ScreenName.tsx"
+import React from 'react'
+import { Dashboard, $ComponentName, ScreenMenu } from '../../components'
+import theme from '../../utils/theme/theme'
+import { FlatList, View } from 'react-native'
+
+const $ScreenName = () => {
+  // flatlist is used to render children to allow the
+  // dashboard header to be sticky in the webview
+  const childrenArray = [<$ComponentName key={0} />]
+
+  return (
+    <>
+      <Dashboard>
+        <ScreenMenu
+          title={'$screenTitle'}
+          bgColor={theme.colors.invoicePrimary}
+          navigateTo="$navigateTo"
+          buttonText="$buttonText"
+        />
+
+        <FlatList
+          style={{
+            flex: 1,
+            width: '100%',
+            backgroundColor: theme.colors.backgroundGrey,
+          }}
+          data={childrenArray}
+          renderItem={({ item }) => <View>{item}</View>}
+          keyExtractor={(item, index) => \`child-\${index}\`}
+        />
+      </Dashboard>
+    </>
+  )
+}
+
+export default $ScreenName
+EOF
+
+# Create the component .tsx file with the specified content
+cat << EOF > "./app/components/$folderName/$componentFolderName/$ComponentName.tsx"
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const $ComponentName = () => {
+  return (
+    <View>
+      <Text>$ComponentName</Text>
+    </View>
+  )
+}
+
+export default $ComponentName
+EOF
+
+echo "Screen file created at ./app/screens/$folderName/$ScreenName.tsx"
+echo "Component file created at ./app/components/$folderName/$ComponentName.tsx"
+
+# Add import and export to screens index.ts
+sed -i '' "/^\/\/$importComment/a\\
+import $ScreenName from './$folderName/$ScreenName'
+" ./app/screens/index.ts
+sed -i '' "/^export {/a\\
+  $ScreenName,
+" ./app/screens/index.ts
+
+# Add import and export to components index.ts
+sed -i '' "/^\/\/$importComment/a\\
+import $ComponentName from './$folderName/$componentFolderName/$ComponentName'
+" ./app/components/index.tsx
+sed -i '' "/^export {/a\\
+  $ComponentName,
+" ./app/components/index.tsx
+
+echo "Import and export added to ./app/screens/index.ts"
+echo "Import and export added to ./app/components/index.tsx"
