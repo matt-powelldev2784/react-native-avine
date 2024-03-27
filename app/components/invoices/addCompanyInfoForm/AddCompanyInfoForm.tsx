c3c492a365ab6df89b15a1b52de6a1682a56c70e
Matthew Powell <matt.powell2784@gmail.com>
2024-03-27 18:36:16 +0000

    feat: install expo document picker

--- app/components/invoices/addCompanyInfoForm/AddCompanyInfoForm.tsx
+++ app/components/invoices/addCompanyInfoForm/AddCompanyInfoForm.tsx
@@ -1,10 +1,27 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { Button, View } from 'react-native'
+import * as DocumentPicker from 'expo-document-picker'
 
 const AddCompanyInfoForm = () => {
+  const selectOneFile = async () => {
+    try {
+      const res = await DocumentPicker.getDocumentAsync({
+        type: 'image/*', // Allowing only images
+      })
+      if (!res.canceled) {
+        console.log('Response : ' + JSON.stringify(res))
+        console.log('URI : ' + res.uri)
+        console.log('Name : ' + res.i)
+        console.log('Size : ' + res.size)
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
-    <View>
-      <Text>AddCompanyInfoForm</Text>
+    <View style={{ margin: 50 }}>
+      <Button title="Select Image" onPress={selectOneFile} />
     </View>
   )
 }
