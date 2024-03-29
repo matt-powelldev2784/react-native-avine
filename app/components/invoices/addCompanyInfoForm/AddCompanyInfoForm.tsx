b59df2bde7ede78f87e3cdb0ac753654bdd1a950
Matthew Powell <matt.powell2784@gmail.com>
2024-03-29 12:02:26 +0000

    feat: add logic to store company logo

--- app/components/invoices/addCompanyInfoForm/AddCompanyInfoForm.tsx
+++ app/components/invoices/addCompanyInfoForm/AddCompanyInfoForm.tsx
@@ -1,27 +1,45 @@
-import React from 'react'
-import { Button, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Button, View, Image } from 'react-native'
 import * as DocumentPicker from 'expo-document-picker'
+import { uploadCompanyLogo } from '../../../db/user/uploadCompanyLogo'
+import { getCompanyLogo } from '../../../db/user/getCompanyLogo'
 
 const AddCompanyInfoForm = () => {
+  const [logoUrl, setLogoUrl] = useState(null)
+
+  useEffect(() => {
+    const fetchLogoUrl = async () => {
+      const url = await getCompanyLogo()
+      console.log('url', url)
+      setLogoUrl(url)
+    }
+
+    fetchLogoUrl()
+  }, [])
+
   const selectOneFile = async () => {
     try {
       const res = await DocumentPicker.getDocumentAsync({
-        type: 'image/*', // Allowing only images
+        type: 'image/jpeg', // Allowing only jpg images
       })
       if (!res.canceled) {
-        console.log('Response : ' + JSON.stringify(res))
-        console.log('URI : ' + res.uri)
-        console.log('Name : ' + res.i)
-        console.log('Size : ' + res.size)
+        console.log('res.assets.uri', res.assets[0].uri)
+        console.log('res', res.output[0].name)
+
+        // Call the function to upload the file to Firebase
+        uploadCompanyLogo(res.assets[0].uri)
       }
-    } catch (err) {
-      console.error(err)
+    } catch (error) {
+      console.error('File selection error: ', error)
     }
   }
 
   return (
     <View style={{ margin: 50 }}>
       <Button title="Select Image" onPress={selectOneFile} />
+      {logoUrl && (
+        <Image source={{ uri: logoUrl }} style={{ width: 200, height: 200 }} />
+      )}
     </View>
   )
 }
---
+++ app/db/user/uploadCompanyLogo.ts
@@ -0,0 +1,48 @@
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
+import { db, auth, storage } from '../../../firebaseConfig'
+import { authError } from '../authError'
+import { doc, setDoc } from '@firebase/firestore'
+
+export const uploadCompanyLogo = async (fileUri: string) => {
+  if (!auth.currentUser) {
+    return authError({ filename: 'uploadCompanyLogo' })
+  }
+
+  try {
+    const storageRef = ref(storage, `images/${auth.currentUser.uid}/logo.jpg`)
+    const response = await fetch(fileUri)
+    const blob = await response.blob()
+    const metadata = {
+      contentType: 'image/jpeg',
+    }
+
+    // Upload the file to Firebase Storage
+    const uploadTask = uploadBytesResumable(storageRef, blob, metadata)
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(
+      'state_changed',
+      null,
+      (error) => {
+        throw new Error(`Error uploading file to Firebase Storage: ${error}`)
+      },
+      async () => {
+        if (!auth.currentUser) {
+          return authError({ filename: 'uploadCompanyLogo' })
+        }
+
+        // Upload completed successfully, get the download URL
+        const downloadURL = await getDownloadURL(uploadTask.snapshot.ref)
+        console.log('File available at', downloadURL)
+
+        // Update the user's document with the logo URL
+        const userDoc = doc(db, 'users', auth.currentUser.uid)
+        await setDoc(userDoc, { logo: downloadURL }, { merge: true })
+
+        return downloadURL
+      },
+    )
+  } catch (error) {
+    throw new Error(`Error in uploadCompanyLogo route: ${error}`)
+  }
+}
