615a5277bfdd6f7401b47ad5a2cbd278dd8d4b06
Matthew Powell <matt.powell2784@gmail.com>
2024-05-02 21:03:54 +0100

    refactor: remove unused variables

--- app/components/clients/clientList/clientListItem/ClientListItem.tsx
+++ app/components/clients/clientList/clientListItem/ClientListItem.tsx
@@ -1,42 +1,22 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React, { useState } from 'react'
+import React from 'react'
 import theme from '../../../../utils/theme/theme'
 import IconButton from '../../../../ui/iconButton/IconButton'
 import { useNavigation } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
 import { RootStackParamList } from '../../../../screens/stackNavigator/StackNavigator'
-import { ConfirmModal } from '../../../../ui'
-import { deleteClient } from '../../../../db/clients/deleteClient'
 import { ClientWithIdT } from '../../../../types/ClientT'
-import usePostApiData from '../../../../utils/hooks/usePostApiData'
 
 const ClientListItem = ({ id, name, contactTel }: ClientWithIdT) => {
-  // state
-  const [modalVisible, setModalVisible] = useState(false)
-
   //hooks
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>()
-  const { postApiIsLoading, setApiFunction } = usePostApiData({
-    onSuccessScreen: 'Clients',
-    refreshScreen: { refresh: true },
-  })
 
-  //functions
-  const handleDeleteClientPress = () => {
-    setModalVisible(true)
-  }
-  const handleConfirmDeleteClientPress = async () => {
-    setApiFunction(() => async () => deleteClient(id))
-  }
   const handleEditClientPress = () => {
     navigation.navigate('EditClient', { clientId: id })
   }
   const handleViewClientPress = () => {
-     navigation.navigate('ClientCardView', { clientId: id })
+    navigation.navigate('ClientCardView', { clientId: id })
   }
-
-  //variables
-  // const maxTextWidth = isLargeWeb ? 500 : 200
 
   return (
     <View style={styles.wrapper}>
@@ -52,11 +32,6 @@ const ClientListItem = ({ id, name, contactTel }: ClientWithIdT) => {
         </View>
 
         <View style={styles.rightContainer}>
-          {/* <IconButton
-            onPress={handleDeleteClientPress}
-            imgSource={require('../../../../../assets/bin.png')}
-            size={35}
-          /> */}
           <IconButton
             onPress={handleViewClientPress}
             imgSource={require('../../../../../assets/eye.png')}
@@ -69,25 +44,17 @@ const ClientListItem = ({ id, name, contactTel }: ClientWithIdT) => {
           />
         </View>
       </View>
-
-      <ConfirmModal
-        modalText={`Are you sure you want to delete client ${name}?`}
-        modalText2={`${name} will be deleted from view but will still be accessible for previously created jobs and invoices.`}
-        onConfirm={handleConfirmDeleteClientPress}
-        onCancel={() => setModalVisible(false)}
-        visible={modalVisible}
-        isLoading={postApiIsLoading}
-      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   wrapper: {
-    width: '100%',
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
+    maxWidth: 800,
+    width: '100%',
   },
   container: {
     flexDirection: 'row',
