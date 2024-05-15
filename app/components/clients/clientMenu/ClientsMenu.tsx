37ecbe60b27542ba5cb0f3ba6cdfbb7965aefb42
Matthew Powell <matt.powell2784@gmail.com>
2024-05-15 20:31:33 +0100

    refactor: move header card styles to stylesheet

--- app/components/clients/clientMenu/ClientsMenu.tsx
+++ app/components/clients/clientMenu/ClientsMenu.tsx
@@ -1,68 +1,41 @@
 import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import theme from '../../../utils/theme/theme'
-import InstructionBox from '../../welcome/components/InstructionBox'
 import { useDeviceType } from '../../../utils/deviceTypes'
+import MenuCard from './components/MenuCard'
 
 const ClientsMenu = () => {
   const { isLargeWeb } = useDeviceType()
 
   return (
     <View style={styles.container}>
+      {/* -------------------- Header---------------------------- */}
       {isLargeWeb ? (
-        <View
-          style={{
-            flexDirection: 'column',
-            alignItems: 'center',
-            flexWrap: 'wrap',
-            gap: 40,
-            width: '100%',
-            height: 400,
-            borderRadius: 12,
-            backgroundColor: 'rgba(0,0,0,0.5)',
-          }}
-        >
+        <View style={styles.headerContainer}>
           <Image
             source={require('../../../../assets/clients.jpg')}
             style={{ width: '100%', height: 400, marginBottom: 8 }}
           />
-          <View
-            style={{
-              position: 'absolute',
-              height: 60,
-              transform: [{ translateY: 170 }],
-              maxWidth: '80%',
-              backgroundColor: 'rgba(0,0,0,0.5)',
-              borderRadius: 12,
-              paddingTop: 8,
-              paddingHorizontal: 16,
-            }}
-          >
-            <Text
-              style={{
-                textAlign: 'center',
-                color: 'white',
-                fontWeight: 'bold',
-                fontSize: 34,
-              }}
-            >
+          <View style={styles.headerTextContainer}>
+            <Text style={styles.headerText}>
               Add client or search client{' '}
               <span style={{ color: theme.colors.secondary }}>Database</span>
             </Text>
           </View>
         </View>
       ) : null}
-      <View style={styles.instructionWrapper}>
-        {/* -------------------- 1. Add Client ---------------------------- */}
-        <InstructionBox
+
+      {/* -------------------- Menu Cards ---------------------------- */}
+      <View style={styles.menuCardContainer}>
+        <MenuCard
           number="1"
           text="Add Client"
           secondaryText="Click here to add a client. Every job requires a client for invoicing purposes."
           navigateTo="AddClient"
           backgroundColor={theme.colors.primary}
         />
-        {/* -------------------- 1. Add Client ---------------------------- */}
-        <InstructionBox
+
+        <MenuCard
           number="2"
           text="Search Clients"
           secondaryText="Click here to add a client. Every job requires a client for invoicing purposes."
@@ -89,7 +62,33 @@ const styles = StyleSheet.create({
     height: '100%',
     flex: 1,
   },
-  instructionWrapper: {
+  headerContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    gap: 40,
+    width: '100%',
+    height: 400,
+    borderRadius: 12,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  headerText: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 34,
+  },
+  headerTextContainer: {
+    position: 'absolute',
+    height: 60,
+    transform: [{ translateY: 170 }],
+    maxWidth: '80%',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    borderRadius: 12,
+    paddingTop: 8,
+    paddingHorizontal: 16,
+  },
+  menuCardContainer: {
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     alignItems: 'center',
