1b70ef0cb3d5d4640aec2992d050667ac63f278e
Matthew Powell <matt.powell2784@gmail.com>
2024-05-16 19:00:15 +0100

    feat: add get all jobs with limit route

---
+++ app/db/jobs/getAllJobsWithLimit.ts
@@ -0,0 +1,77 @@
+import {
+  doc,
+  collection,
+  getDocs,
+  query,
+  where,
+  limit,
+  startAfter,
+  orderBy,
+  getCountFromServer,
+} from 'firebase/firestore'
+import { db, auth } from '../../../firebaseConfig'
+import { authError } from '../authError'
+import { DocumentSnapshot } from 'firebase/firestore'
+import { JobWithIdT } from '../../types/JobT'
+
+export const getAllJobsWithLimit = async (
+  lastVisibleDocument: DocumentSnapshot | null = null,
+) => {
+  if (!auth.currentUser) {
+    return authError({ filename: 'getAllJobsWithLimit' })
+  }
+
+  try {
+    const userDoc = doc(db, 'users', auth.currentUser.uid)
+    const jobsCollection = collection(userDoc, 'jobs')
+
+    //get clients data
+    let q
+    if (lastVisibleDocument) {
+      q = query(
+        jobsCollection,
+        where('isDeleted', '!=', true),
+        orderBy('jobName'),
+        startAfter(lastVisibleDocument),
+        limit(10),
+      )
+    } else {
+      q = query(
+        jobsCollection,
+        where('isDeleted', '!=', true),
+        orderBy('jobName'),
+        limit(10),
+      )
+    }
+
+    const querySnapshot = await getDocs(q)
+
+    const jobs = querySnapshot.docs.map((job) => ({
+      id: job.id,
+      ...job.data(),
+    })) as JobWithIdT[]
+
+    const sortedJobs = jobs.sort((a, b) => a.jobName.localeCompare(b.jobName))
+
+    // get document count
+    const countQuery = query(
+      jobsCollection,
+      where('isDeleted', '!=', true),
+      orderBy('jobName'),
+    )
+    const snapshot = await getCountFromServer(countQuery)
+    const count = snapshot.data().count
+
+    const data = {
+      jobs: sortedJobs,
+      lastVisible: querySnapshot.docs[querySnapshot.docs.length - 1],
+      count,
+    }
+
+    return data
+  } catch (error) {
+    throw new Error(
+      `Error getting clients at getAllJobsWithLimit route: ${error}`,
+    )
+  }
+}
