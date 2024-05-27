ec696d90f9c50ca8788aa6cb9288a7f098d76fd5
Matthew Powell <matt.powell2784@gmail.com>
2024-05-27 16:48:12 +0100

    feat: restyle round search full functionality is incomplete

--- app/components/rounds/roundList/RoundList.tsx
+++ app/components/rounds/roundList/RoundList.tsx
@@ -1,101 +1,274 @@
-import { View, FlatList, StyleSheet } from 'react-native'
-import React from 'react'
-import RoundCard from './components/RoudnCard'
-import { getAllRoundsWithRelatedJobs } from '../../../db/rounds/withRelatedJobs/getAllRoundsWithRelatedJobs'
-import { RoundWithJobT } from '../../../types/RoundT'
-import { Loading } from '../../../ui'
-import ErrorNoData from './components/ErrorNoData'
-import { useDeviceType } from '../../../utils/deviceTypes'
-import { useRoute } from '@react-navigation/native'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  ViewStyle,
+  Image,
+} from 'react-native'
+import React, { useState } from 'react'
+import InputField from '../../../ui/formElements/InputField'
+import Dropdown from '../../../ui/formElements/DropDown'
+import RoundListItem from './components/RoundListItem'
+import Button from '../../../ui/button/Button'
 import theme from '../../../utils/theme/theme'
-import useGetApiData from '../../../utils/hooks/useGetApiData'
+import useFormikSearch from './hooks/useFormikSearch'
+import { useDeviceType } from '../../../utils/deviceTypes'
+import useResetSearchOnFocus from '../../../utils/hooks/useResetSearchOnFocus'
+import NoDataFound from './components/NoDataFound'
+import { RoundWithIdT } from '../../../types/RoundT'
 
 const RoundList = () => {
-  const route = useRoute()
-  const { isSmallWeb, isLargeWeb, isNative } = useDeviceType()
-  const { getApiIsLoading, data } = useGetApiData({
-    route,
-    apiFunction: async () => getAllRoundsWithRelatedJobs(),
-  })
-  const roundData = data as RoundWithJobT[]
+  //state
+  const [searchIsActive, setSearchIsActive] = useState<boolean>(false)
 
-  if (getApiIsLoading) {
-    return <Loading loadingText={'Loading rounds list...'} />
+  //hooks
+  const { isLargeWeb } = useDeviceType()
+  useResetSearchOnFocus(() => {
+    setRoundData([])
+    setLastVisibleDocument(null)
+    setSearchIsActive(false)
+  })
+  const {
+    searchApiIsLoading,
+    formik,
+    roundData,
+    setRoundData,
+    setLastVisibleDocument,
+    docCount,
+  } = useFormikSearch()
+
+  console.log('roundData', roundData)
+
+  //functions
+  const handleSearchAllRoundsPress = () => {
+    formik.setFieldValue('findAll', true)
+    resetSearchForm()
+    formik.handleSubmit()
+    setSearchIsActive(true)
+  }
+  const handleSearchPress = async () => {
+    formik.setFieldValue('findAll', false)
+    resetSearchForm()
+    formik.handleSubmit()
+    setSearchIsActive(true)
+  }
+  const handleMoreResultsPress = async () => {
+    formik.handleSubmit()
+  }
+  const resetSearchForm = () => {
+    setRoundData([])
+    setLastVisibleDocument(null)
+    setSearchIsActive(false)
   }
 
-  if (!roundData || roundData.length === 0) {
-    return <ErrorNoData />
-  }
-  const RoundCards = roundData?.map((round) => {
-    return <RoundCard {...round} key={round.id} />
-  })
+  //variables
+  const roundDataHasLength = roundData.length > 0
+  const RoundCards = roundDataHasLength
+    ? roundData.map((round: RoundWithIdT) => {
+        return <RoundListItem {...round} key={round.id} />
+      })
+    : null
+  const allDataReturned = docCount === roundData.length
+
+  //styles
+  const deviceHeight =
+    searchIsActive && !isLargeWeb ? 350 : Dimensions.get('window').height - 95
+  const containerStyle: ViewStyle = !isLargeWeb
+    ? { flexDirection: 'column' }
+    : { flexDirection: 'row' }
+  const searchContainerStyle: ViewStyle = isLargeWeb
+    ? { width: '30%' }
+    : { width: '100%' }
+  const searchResultsStyle: ViewStyle = isLargeWeb
+    ? { width: '70%' }
+    : { width: '100%', paddingTop: 20 }
 
   return (
-    <View style={styles.listContainer}>
-      {roundData && isLargeWeb ? (
-        <View style={styles.largeWebCards}>{RoundCards}</View>
-      ) : null}
-      {roundData && isSmallWeb ? (
-        <View style={styles.smallDeviceCards}>{RoundCards}</View>
+    <View style={[styles.container, containerStyle]}>
+      {/* -------------------- Header---------------------------- */}
+      {!searchIsActive || (searchIsActive && !isLargeWeb) ? (
+        <View style={[styles.headerContainer, { height: deviceHeight }]}>
+          <Image
+            source={require('../../../../assets/jobs.jpg')}
+            style={{ width: '100%', height: deviceHeight, marginBottom: 8 }}
+          />
+        </View>
       ) : null}
 
-      {roundData && isNative ? (
-        <FlatList
-          style={styles.smallDeviceCards}
-          data={roundData}
-          renderItem={({ item }) => <RoundCard {...item} />}
-          keyExtractor={(item) => item.id}
-          ListFooterComponent={<View style={styles.flatlistFooter} />}
-        />
+      {/* -------------------- Search Container ---------------------------- */}
+      <View style={[styles.searchContainer, searchContainerStyle]}>
+        <View style={styles.searchTitleContainer}>
+          <Image
+            source={require('../../../../assets/search_white.png')}
+            style={{ width: 25, height: 25 }}
+          />
+          <Text style={styles.searchTitle}>
+            Search{' '}
+            <Text
+              style={[styles.searchTitle, { color: theme.colors.secondary }]}
+            >
+              Rounds
+            </Text>
+          </Text>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Dropdown
+            formik={formik}
+            name="searchField"
+            placeholder="Search By"
+            title="Search By"
+            options={[{ label: 'Name', value: '_searchName' }]}
+            imageName={'notes'}
+          />
+          <InputField
+            formik={formik}
+            name="searchTerm"
+            placeholder="Search Term"
+            title="Search Term"
+            imageName={'search'}
+          />
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={handleSearchAllRoundsPress}
+            text="Find All Rounds"
+            isLoading={searchApiIsLoading}
+            backgroundColor={theme.colors.buttonSecondary}
+          />
+          <Button
+            onPress={handleSearchPress}
+            text="Search"
+            isLoading={searchApiIsLoading}
+          />
+        </View>
+      </View>
+
+      {/* -------------------- Job Cards ---------------------------- */}
+      {searchIsActive ? (
+        <View style={[styles.searchResultsContainer, searchResultsStyle]}>
+          {isLargeWeb ? (
+            <View style={styles.searchTitleContainer}>
+              <Image
+                source={require('../../../../assets/search_white.png')}
+                style={{ width: 25, height: 25 }}
+              />
+              <Text style={styles.searchTitle}>
+                Search{' '}
+                <Text
+                  style={[
+                    styles.searchTitle,
+                    { color: theme.colors.secondary },
+                  ]}
+                >
+                  Results
+                </Text>
+              </Text>
+            </View>
+          ) : null}
+
+          {RoundCards}
+
+          {docCount === 0 ? <NoDataFound /> : null}
+
+          {roundDataHasLength ? (
+            <View style={styles.buttonContainer}>
+              <Button
+                onPress={resetSearchForm}
+                text="Reset Search Form"
+                isLoading={false}
+                backgroundColor={theme.colors.buttonSecondary}
+              />
+              <Button
+                onPress={handleMoreResultsPress}
+                text={allDataReturned ? 'No More Results' : 'Next 10 Results'}
+                isLoading={searchApiIsLoading}
+                opacity={allDataReturned ? 0.75 : 1}
+                disabled={allDataReturned}
+              />
+            </View>
+          ) : null}
+
+          <View style={styles.footer} />
+        </View>
       ) : null}
     </View>
   )
 }
 
+export default RoundList
+
 const styles = StyleSheet.create({
-  listContainer: {
+  container: {
+    position: 'relative',
+    flexDirection: 'row',
+    justifyItems: 'center',
+    alignItems: 'flex-start',
+    width: '100%',
+    height: '100%',
     flex: 1,
+    backgroundColor: theme.colors.tertiaryBlue,
+  },
+  headerContainer: {
+    position: 'absolute',
+    width: '100%',
+    right: 0,
+  },
+  searchContainer: {
+    width: '30%',
+    height: '100%',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    width: '100%',
-    backgroundColor: theme.colors.backgroundGrey,
+    paddingHorizontal: 5,
+    minWidth: 250,
+    minHeight: 350,
+    flex: 1,
+    transform: [{ translateY: -5 }],
   },
-  largeWebCards: {
-    display: 'grid' as any,
-    gridTemplateColumns: 'repeat(auto-fill, minmax(330px, 1fr))' as any,
-    width: '100vw' as any,
-    justifyItems: 'center',
+  searchTitleContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    gap: 20,
-    padding: 20,
+    justifyContent: 'center',
+    gap: 16,
+    marginBottom: 30,
+    marginTop: 30,
   },
-  smallDeviceCards: {
-    width: '100%',
-    paddingTop: 10,
-    paddingBottom: 70,
-    paddingHorizontal: '2.5%',
+  searchTitle: {
+    color: theme.colors.white,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
-  flatlistFooter: {
-    height: 20,
+  inputContainer: {
+    width: '90%',
+    maxWidth: 600,
   },
-  button: {
+  buttonContainer: {
+    display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
-    paddingVertical: 8,
-    paddingHorizontal: 32,
-    backgroundColor: theme.colors.primary,
-    borderRadius: 8,
+    flexWrap: 'wrap',
     width: '100%',
-    maxWidth: 270,
-    margin: 8,
-    gap: 4,
+    maxWidth: 600,
+    gap: 8,
+    zIndex: -1,
   },
-  buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 16,
+  searchResultsContainer: {
+    height: '100%',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    backgroundColor: theme.colors.tertiaryBlue,
+    marginBottom: 4,
+    zIndex: -1,
+    transform: [{ translateY: -5 }],
+  },
+  footer: {
+    height: 80,
+    width: '100%',
+    minWidth: 300,
   },
 })
-
-export default RoundList
