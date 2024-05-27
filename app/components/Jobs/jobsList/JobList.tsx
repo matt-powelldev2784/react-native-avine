c8817527f2d5cb497e36e30e21013837618444c4
Matthew Powell <matt.powell2784@gmail.com>
2024-05-27 14:05:00 +0100

    feat: move use reset search to focus to global utils folder

--- app/components/Jobs/jobsList/JobList.tsx
+++ app/components/Jobs/jobsList/JobList.tsx
@@ -15,7 +15,7 @@ import theme from '../../../utils/theme/theme'
 import useFormikSearch from './hooks/useFormikSearch'
 import { JobWithIdT } from '../../../types/JobT'
 import NoDataFound from './components/NoDataFound'
-import useResetSearchOnFocus from './hooks/useResetSearchOnFocus'
+import useResetSearchOnFocus from '../../../utils/hooks/useResetSearchOnFocus'
 import { useDeviceType } from '../../../utils/deviceTypes'
 
 const JobList = () => {
