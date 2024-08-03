05a22f4b59f5bfbd30a2a1182f6790ff96ff5833
Matthew Powell <matt.powell2784@gmail.com>
2024-08-03 19:33:57 +0100

    feat: change last book date to a time stamp

--- app/db/planner/addRound/addRecurringData.ts
+++ app/db/planner/addRound/addRecurringData.ts
@@ -1,7 +1,8 @@
-import { doc, getDoc, setDoc } from 'firebase/firestore'
+import { doc, getDoc, setDoc, Timestamp } from 'firebase/firestore'
 import { db, auth } from '../../../../firebaseConfig'
 import { getRound } from '../../rounds/getRound'
 import { getRecurringDatesTwoYearsAhead } from '../../../utils/getRecurringDates2YearsAhead'
+import { convertDbDateToDateString } from '../../../utils/convertDbDateToDateString'
 
 interface addRecurringRoundT {
   roundId: string
@@ -39,10 +40,19 @@ export const addRecurringData = async ({
 
     const recurringDates = getRecurringDatesTwoYearsAhead(date, round.frequency)
 
+    const createTimeStanp = (date: string) => {
+      const dateString = convertDbDateToDateString(date)
+      const timestamp = new Timestamp(new Date(dateString).getTime() / 1000, 0)
+      return timestamp
+    }
+    const lastDateBooked = createTimeStanp(
+      recurringDates[recurringDates.length - 1],
+    )
+
     await setDoc(recurringRoundDocRef, {
       roundId: roundId,
       startDate: date,
-      lastDateBooked: recurringDates[recurringDates.length - 1],
+      lastDateBooked: lastDateBooked,
       frequency: round.frequency,
       recurringDates: recurringDates,
     })
